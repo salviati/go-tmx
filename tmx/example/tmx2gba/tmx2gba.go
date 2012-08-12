9c5a3435cd4f734a83b06fd2f7ba2dbb26180936
Utkan Güngördü <utkan@freeconsole.org>
2012-08-12 13:08:14 +0900

    go fmt

--- tmx/example/tmx2gba/tmx2gba.go
+++ tmx/example/tmx2gba/tmx2gba.go
@@ -24,20 +24,20 @@
   Converts a TMX file to files which can be loaded to GBA.
 
   The TMX is assumed to have only one tileset.
-  
+
   Data for each layer will be written in separate files. If the map "hello.tmx" has two layers "BG2" and "BG3",
   the data will be written into "hello.BG1.layer" "hello.BG2.layer".
   Depending on the Properties field of each layer, an encoding will take place:
-  
+
   Bitmap=true: The layer will be encoded into a 1-bit-per-tile stream. NilTiles will be encoded as 0, others as 1.
   Useful for generating obstruction layer data in a compact form.
-  
+
   NilTile=ID: NilTile is ordinarily encoded into NTiles by default (that is just out of the valid range of tiles).
   This property will override the default.
-  
+
   Compression=Method where method is one of LZ77, RLE, Huffman4, Huffman8. Will compress the layer data.
   (Huffman4 is not implemented yet)
-  
+
   The size of a layer file is MapWidth*MapHeight*2 byte for layers when Bitmap=true is not set.
   Size of a bitmap layer is MapWidth*MapHeight/8
 */
@@ -45,32 +45,32 @@ package main
 
 import (
 	"encoding/binary"
+	"errors"
 	"flag"
-	"github.com/salviati/go-tmx/tmx"
 	"github.com/salviati/gbacomp"
-	"path/filepath"
+	"github.com/salviati/go-tmx/tmx"
+	"io"
 	"log"
 	"os"
-	"io"
-	"errors"
+	"path/filepath"
 	"strconv"
 )
 
 const (
-	HFlip = 1 << 10
-	VFlip = 1 << 11
-	Magic = "GBAMAP01"
+	HFlip  = 1 << 10
+	VFlip  = 1 << 11
+	Magic  = "GBAMAP01"
 	TMXExt = ".tmx"
 )
 
 var (
-	InvalidNumberOfTilesets = errors.New("The number of tilesets used in a map should exactly be 1.")
-	WrongFileExtension = errors.New("The file extension must be .tmx")
-	PropertyNotUnique = errors.New("Layer Property is not unique")
-	PropertyUnavailable = errors.New("Property does not exist")
+	InvalidNumberOfTilesets  = errors.New("The number of tilesets used in a map should exactly be 1.")
+	WrongFileExtension       = errors.New("The file extension must be .tmx")
+	PropertyNotUnique        = errors.New("Layer Property is not unique")
+	PropertyUnavailable      = errors.New("Property does not exist")
 	InvalidCompressionMethod = errors.New("Invalid compression method")
 
-	CompressionMethods = map[string]gbacomp.Method{"LZ77": gbacomp.LZ77, "RLE": gbacomp.RLE, /*"Huffman4":gbacomp.Huffman4,*/ "Huffman8":gbacomp.Huffman8 }
+	CompressionMethods = map[string]gbacomp.Method{"LZ77": gbacomp.LZ77, "RLE": gbacomp.RLE /*"Huffman4":gbacomp.Huffman4,*/, "Huffman8": gbacomp.Huffman8}
 )
 
 func doTMX(filename string) error {
@@ -82,37 +82,41 @@ func doTMX(filename string) error {
 	if len(m.Tilesets) != 1 {
 		return InvalidNumberOfTilesets
 	}
-	
+
 	ext := filepath.Ext(filename)
 	if ext != TMXExt {
 		return WrongFileExtension
 	}
 	filenameBare := filename[:len(filename)-len(ext)-1]
 
-
-
 	nilTileDefault := uint16(len(m.Tilesets[0].Tiles))
-	
-	getProp := func (p *tmx.Properties, name string) (value string, err error) {
+
+	getProp := func(p *tmx.Properties, name string) (value string, err error) {
 		values := p.Get(name)
-		if len(values) > 1 { err = PropertyNotUnique; return }
-		if len(value) == 0 { err = PropertyUnavailable; return}
+		if len(values) > 1 {
+			err = PropertyNotUnique
+			return
+		}
+		if len(value) == 0 {
+			err = PropertyUnavailable
+			return
+		}
 		value = values[0]
 		return
 	}
-	
+
 	save := func(l *tmx.Layer, w io.WriteCloser) error {
 		defer w.Close()
 		b := w
-		
+
 		nilTile := nilTileDefault
 		nilTileString, _ := getProp(&l.Properties, "NilTile")
-		nilTileNew, err := strconv.ParseUint(nilTileString,10,16)
+		nilTileNew, err := strconv.ParseUint(nilTileString, 10, 16)
 		if err == nil {
 			nilTile = uint16(nilTileNew)
 		}
 
-		compression, _ := getProp(&l.Properties,"Compression")
+		compression, _ := getProp(&l.Properties, "Compression")
 		if compression != "" {
 			compressionMethod, ok := CompressionMethods[compression]
 			if !ok {
@@ -121,7 +125,6 @@ func doTMX(filename string) error {
 			b = gbacomp.NewCompressor(w, compressionMethod)
 			defer b.Close()
 		}
-		
 
 		i := 0
 		for y := 0; y < m.Height; y++ {
@@ -141,7 +144,7 @@ func doTMX(filename string) error {
 				if l.DecodedTiles[i]&tmx.GIDVerticalFlip != 0 {
 					tid |= VFlip
 				}
-				// TODO(utkan); palette bank
+				// TODO(utkan): palette bank
 				err = binary.Write(b, binary.LittleEndian, tid)
 				if err != nil {
 					return err
@@ -152,7 +155,7 @@ func doTMX(filename string) error {
 		}
 		return nil
 	}
-	
+
 	saveBitmap := func(l *tmx.Layer, w io.WriteCloser) error {
 		defer w.Close()
 
@@ -177,18 +180,18 @@ func doTMX(filename string) error {
 		return nil
 	}
 
-	for i:=0; i<len(m.Layers); i++ {
+	for i := 0; i < len(m.Layers); i++ {
 		l := &m.Layers[i]
-		
-		bitmap, err := getProp(&l.Properties,"Bitmap")
-		
-		name := filenameBare+"." + l.Name + ".layer"
+
+		bitmap, err := getProp(&l.Properties, "Bitmap")
+
+		name := filenameBare + "." + l.Name + ".layer"
 		f, err := os.OpenFile(name, os.O_WRONLY, 0666)
 		if err != nil {
 			return err
 		}
 		defer f.Close()
-		
+
 		if bitmap == "true" {
 			if err := saveBitmap(l, f); err != nil {
 				return err
@@ -199,7 +202,6 @@ func doTMX(filename string) error {
 			}
 		}
 	}
-
 
 	return nil
 }
