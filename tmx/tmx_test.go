f4853652dddc0ffedf5e6e4a9b48dde67f882ef8
Utkan Güngördü <utkan@freeconsole.org>
2012-12-05 20:15:54 +0900

    merge test-fix, and properties related stuff from kyleconroy

--- tmx/tmx_test.go
+++ tmx/tmx_test.go
@@ -73,15 +73,14 @@ func TestRead(t *testing.T) {
 		}
 	}
 
-	chkLayerData := func(originalGIDs []GID, decodedTiles []*DecodedTile) {
+	chkLayerData := func(originalData, readData []GID) {
 		if len(readData) != len(originalData) {
 			t.Error("Wrong number of tiles", len(readData), "Should be", len(originalData))
 			return
 		}
 
-		for i, tile := range decodedTiles {
-			originalTile, _ := m.DecodeGID(originalData[i])
-			if originalTile.ID != tile.ID {
+		for i, gid := range readData {
+			if originalData[i] != gid {
 				t.Error("Wrong gid at position", i)
 				return
 			}
